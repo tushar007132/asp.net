3a025b3dab99dc67e4ccebc5770abc27144a7115
tushar007132 <tusharkumar0308@gmail.com>
2022-07-26 20:35:42 +0530

    Add files via upload

---
+++ day2/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApplicationEmp.Controllers
+{
+    public class Employee
+    {
+        public int Empid
+        {
+            get; set;
+        }
+
+        public string Ename
+        {
+            get; set;
+        }
+
+        public string Ejob
+        {
+            set; get;
+        }
+
+        public int Esal
+        {
+            get; set;
+        }
+
+        public int Edeptno
+        {
+            get; set;
+        }
+    }
+}
---
+++ day2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationEmp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
